1a2f9199a877eacb3741c1359288fc21a1121031
Chae Min Lim <coals722@naver.com>
2024-09-12 14:33:27 +0900

    setting: initial

---
+++ src/main/java/backend/budgetwise/BudgetWiseApplication.java
@@ -0,0 +1,13 @@
+package backend.budgetwise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetWiseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BudgetWiseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/backend/budgetwise/BudgetWiseApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.budgetwise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetWiseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
